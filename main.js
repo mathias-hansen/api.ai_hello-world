75538fff5589cc65775f84a4c8a3e45cd27b2daa
mathias hansen <mathias@mathiass-MacBook-Pro.local>
2017-01-30 16:06:59 +0100

    /hello-world route

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const express = require("express")
 
 const app = express()
 
-app.get("/hello-world")
+app.all("/hello-world", (req, res) => {
+    let body = ""
+
+    console.log(req.method, "/hello-world")
+    
+    res.on("data", chunk => body += chunk.toString())
+        .on("end", () => console.log(body))
+        .on("error", e => {throw e})
+})
 
 app.listen(8888)
