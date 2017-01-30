936c8edebb98fe44072505a5b22c946d772105da
mathias hansen <mathias@mathiass-MacBook-Pro.local>
2017-01-30 16:12:55 +0100

    /hello-world end

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ app.all("/hello-world", (req, res) => {
     let body = ""
 
     console.log(req.method, "/hello-world")
-    
+
     res.on("data", chunk => body += chunk.toString())
-        .on("end", () => console.log(body))
+        .on("end", () => {
+            console.log(body)
+            res.end("OK")
+        })
         .on("error", e => {throw e})
 })
 
